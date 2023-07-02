ccd947cfb653286599784d093ab3728ee0bed0ca
Faraz <farazghoreishy@gmail.com>
2023-07-02 23:48:01 +0330

    added dynamic category and dynamic ui integration

--- main.py
+++ main.py
@@ -10,13 +10,27 @@ from ui_main_window import Ui_MainWindow
 from ui_login import Ui_Dialog as  Login_Ui_Dialog
 from ui_signup import Ui_Dialog as SignUP_Ui_Dialog
 from user import User
+from database import Database
 
 class Window(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.setupUi(self)
+        self.setupUi(self, self.getCategories())
         self.connectSignalsSlots()
+        
 
+    def getCategories(self):
+        db = Database()
+        cursor = db.database.cursor()
+        query = "SELECT DISTINCT category FROM items"
+        cursor.execute(query)
+        categories = cursor.fetchall()
+        cursor.close()
+        db.database.close()
+
+        categories = [category[0] for category in categories]
+        return categories
+    
     def connectSignalsSlots(self):
         self.login_pushButton.clicked.connect(self.executeLogin)
         self.loginMessage_pushButton.clicked.connect(self.executeLogin)
@@ -46,7 +60,8 @@ class LoginDialog(QDialog, Login_Ui_Dialog):
         username = self.username_lineEdit.text()
         password = self.password_lineEdit.text()
         user = User.login(username, password)
-
+        print(user)
+        
     def executeSignUp(self):
         dialog = SignUpDialog()
         dialog.exec()
